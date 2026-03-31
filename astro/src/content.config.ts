23541f085b5baf9863fd5f367361d6d61a637b8a
Sen Hongo <sen@legendofcode.com>
2026-04-01 00:45:21 +0900

    🤘 re-define tag types

--- astro/src/content.config.ts
+++ astro/src/content.config.ts
@@ -1,9 +1,9 @@
-import { defineCollection, z } from "astro:content";
-import { glob } from "astro/loaders";
+import { defineCollection, z } from 'astro:content';
+import { glob } from 'astro/loaders';
 
 const blog = defineCollection({
   // Load Markdown and MDX files in the `src/content/blog/` directory.
-  loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
+  loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
   // Type-check frontmatter using a schema
   schema: () =>
     z.object({
@@ -14,17 +14,14 @@ const blog = defineCollection({
       // Transform string to Date object
       // date: z.coerce.date(),
       // updatedDate: z.coerce.date().optional(),
-      tags: z.array(z.string()).nullish(),
+      tags: z.array(z.string()).default([]),
       category: z.string().nullish(),
       series: z.string().nullish(),
     }),
 });
 
-const tag = defineCollection({
-  schema: () =>
-    z.object({
-      tag: z.string(),
-    }),
+const tags = defineCollection({
+  schema: () => z.array(z.string()).default([]),
 });
 
-export const collections = { blog, tag };
+export const collections = { blog, tags };
