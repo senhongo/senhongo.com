1c07ca9cd66fef1da15b5141807c82f6a7b566e6
Sen <11208360+senhongo@users.noreply.github.com>
2026-04-03 17:56:55 +0900

    Update footer nav contents (#64)
    * 🤘 sitemap
    
    * 🤘 update styling

---
+++ astro/src/content/sitemap.ts
@@ -0,0 +1,65 @@
+export const sitemap = [
+  {
+    section: 'Personal',
+    links: [
+      {
+        label: 'About',
+        url: '/about',
+      },
+      {
+        label: 'Now',
+        url: '/now',
+      },
+      {
+        label: 'Contact',
+        url: '/contact',
+      },
+      {
+        label: 'GitHub',
+        url: 'https://github.com/senhongo',
+      },
+    ],
+  },
+  {
+    section: 'Blog',
+    links: [
+      {
+        label: 'Blog',
+        url: '/blog',
+      },
+      {
+        label: 'Categories',
+        url: '/categories',
+      },
+      {
+        label: 'Series',
+        url: '/series',
+      },
+      {
+        label: 'Tags',
+        url: '/tags',
+      },
+      {
+        label: 'Recipes',
+        url: '/categories/recipe',
+      },
+    ],
+  },
+  {
+    section: 'Millefeuille',
+    links: [
+      {
+        label: 'Millefeuille',
+        url: 'https://millefeuille.app',
+      },
+      {
+        label: 'Changelog',
+        url: 'https://get.millefeuille.app/changelog',
+      },
+      {
+        label: 'Docs',
+        url: 'https://docs.millefeuille.app',
+      },
+    ],
+  },
+];
