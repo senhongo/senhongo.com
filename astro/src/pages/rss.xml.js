43a6f5f569e1313d4c70445ccc223664064283a4
Sen Hongo <sen@legendofcode.com>
2026-06-19 02:45:04 +0900

    🔧 rss sorting

--- astro/src/pages/rss.xml.js
+++ astro/src/pages/rss.xml.js
@@ -3,7 +3,7 @@ import rss from '@astrojs/rss';
 import { SITE_DESCRIPTION, SITE_TITLE } from '@/consts';
 
 export async function GET(context) {
-  const posts = await getCollection('blog').sort((a, b) =>
+  const posts = (await getCollection('blog')).sort((a, b) =>
     b.data.date.localeCompare(a.data.date, undefined, { numeric: true }),
   );
 
